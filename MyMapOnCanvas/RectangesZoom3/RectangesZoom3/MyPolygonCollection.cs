26e2ae7000abd92ebdd57df5aa52899dd258c62e
stormbringer-s@mail.ru <stormbringer-s@mail.ru>
2015-12-31 13:04:41 +0400

    incap polygons into collection

---
+++ MyMapOnCanvas/RectangesZoom3/RectangesZoom3/MyPolygonCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RectangesZoom3
+{
+    class MyPolygonCollection
+    {
+        private readonly Map _map;
+        List<MyPolygon> polygons = new List<MyPolygon>();
+
+        public MyPolygonCollection(Map map)
+        {
+            _map = map;
+        }
+
+        public void Click(Point mouse)
+        {
+            if (active != null)
+            {
+                var thumb = active.AddThumb(mouse, _map);
+                _map.Children.Add(thumb);
+            }
+        }
+
+        public void Start(sbyte mark)
+        {
+            active = new MyPolygon(mark);
+        }
+
+        public void End()
+        {
+            if (active != null)
+            {
+                active.MakePolygon(_map);
+                polygons.Add(active);
+                active = null;
+            }
+        }
+        MyPolygon active;
+        public void Zoom(double scale, Point mouse)
+        {
+            foreach (var p in polygons)
+            {
+                p.Zoom(scale, mouse);
+            }
+            if (active != null)
+            {
+                active.Zoom(scale, mouse);
+            }
+        }
+
+        public void Move(Vector v)
+        {
+            foreach (var p in polygons)
+            {
+                p.Move(v);
+            }
+            if (active != null)
+            {
+                active.Move(v);
+            }
+        }
+    }
+}
--- MyMapOnCanvas/RectangesZoom3/RectangesZoom3/RegionOverlay.cs
+++ MyMapOnCanvas/RectangesZoom3/RectangesZoom3/RegionOverlay.cs
@@ -1,51 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace RectangesZoom3
 {
     class RegionOverlay : OverlayBase
     {
-        public RegionOverlay(Map map) : base(map)
+        public RegionOverlay(Map map)
+            : base(map)
         {
+polygons = new MyPolygonCollection(_map);
         }
+
+        private MyPolygonCollection polygons;
 
         public override void Click(Point mouse)
         {
-            if (active == null)
-                return;
-            var thumb = active.AddThumb(mouse, _map);
-            _map.Children.Add(thumb);
+            polygons.Click(mouse);
         }
 
-        MyPolygon active;
+
+
 
         public void StartRegion()
         {
-            active = new MyPolygon(1);
+            polygons.Start(1);
         }
+
 
         public void EndRegion()
         {
-            active.MakePolygon(_map);
+            polygons.End();
         }
 
         private byte zoomFactor = 2;
 
         public override void OnViewPortChange(Rect oldvp, Rect newvp, byte currentZoom, byte newZoom, Point mouse)
         {
-            if (active != null)
+            var scaleMultiplier = Math.Pow(zoomFactor, newZoom - currentZoom);
+
+            if (scaleMultiplier != 1)
             {
-                var scaleMultiplier = Math.Pow(zoomFactor, newZoom - currentZoom);
-                if (scaleMultiplier != 1)
-                {
-                    active.Zoom(scaleMultiplier, mouse);
-                }
-                else
-                {
-                    var vector = newvp.TopLeft - oldvp.TopLeft;
-                    active.Move(vector);
-                }
+                polygons.Zoom(scaleMultiplier, mouse);
             }
+            else
+            {
+                var vector = newvp.TopLeft - oldvp.TopLeft;
+                polygons.Move(vector);
+            }
+
         }
     }
 }
